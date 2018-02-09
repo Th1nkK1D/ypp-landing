46e01a0954a75c93e76119b311c70eb787d2ce81
Withee Poositasai <witheep@gmail.com>
2018-02-09 22:16:29 +0700

    Update navbar/landing section

--- src/App.vue
+++ src/App.vue
@@ -2,45 +2,44 @@
   <div id="app">
     <!-- Navbar -->
     <div class="navbar">
-      <div class="columns">
-        <div class="column icon">
-          <div class="triangle"></div>
-          <object type="image/svg+xml" data="/static/logoypp.svg"></object>
-        </div>
-
-        <!-- Menu -->
-        <div class="column is-narrow">
-          <div class="columns">
-            <div class="column is-narrow">Event</div>
-            <div class="column is-narrow">Speaker</div>
-            <div class="column is-narrow">Supporter</div>
-          </div>
-        </div><!-- End of Menu -->
+      <div class="icon">
+        <div class="triangle"></div>
+        <object type="image/svg+xml" data="/static/logoypp.svg"></object>
       </div>
+
+      <!-- Menu -->
+      <div class="menu">
+        <a class="r-line">EVENTS</a>
+        <a class="r-line">SPEAKERS</a>
+        <a>SUPPORTERS</a>
+      </div><!-- End of Menu -->
     </div><!-- End of Navbar -->
 
     <!-- Heading -->
-    <section class="section heading">
-      <div class="columns">
+    <div class="heading">
+      <div class="columns is-gapless">
         <div class="column is-4"></div>
         <div class="column">
           <h1>Young Passionate</h1>
           <h1>Program</h1>
 
-          <p>With the exception of Nietzsche, no other madman has contributed so much to human sanity as has Louis Althusser.</p>
+          <p>Ignite your passion. Surround with young talent. Informative workshop from experience mentor.</p>
+          <br>
+          <button class="btn-prim">JOIN US</button>
         </div>
+        <div class="column is-1"></div>
       </div>
-    </section>
+    </div>
     <!-- End of Heading -->
 
-    <section class="section scenter">
+    <div class="scenter">
       <h2 class="hyellow">EVENTS</h2>
-    </section>
+    </div>
 
-    <section class="section scenter">
+    <div class="scenter">
       <h2 class="hgreen">ABOUT US</h2>
       <p>Ever wondered how some graphic designers always manage to produce beautiful looking designs for their brochures, website designs, logo designs? Talent…yes, it helps but there are a handful of more important things you can do that will have even complete beginners producing award winning design.</p>
-    </section>
+    </div>
   </div>
 </template>
 
@@ -58,9 +57,10 @@ export default {
   @import "./assets/fonts/fonts.scss";
 
   body {
-    background: linear-gradient(-15deg, #093038 ,#260933);
+    background: linear-gradient(-15deg, #093038, #260933, #260933);
     color: white;
     font-family: "cloud";
+    font-size: 18px;
     padding: 0;
     margin: 0;
   }
@@ -77,9 +77,21 @@ export default {
     }
   }
 
+  .btn-prim {
+    background: linear-gradient(-100deg, #00769b, #022088);;
+    color: white;
+    font-size: 16px;
+    font-weight: bold;
+    height: 50px;
+    width: 200px;
+    border-radius: 25px;
+    filter: drop-shadow(2px 2px 5px rgba(0,0,0,0.5));
+  }
+
   .navbar {
     width: 100vw;
     position: fixed;
+    text-align: right;
 
     .icon {
       position: relative;
@@ -90,17 +102,32 @@ export default {
         left: 0;
         width: 0;
         height: 0;
-        border-top: 70vh solid #281853;
-        border-right: 70vh solid transparent;
-        filter: drop-shadow(0 0 2px white);
+        border-top: 30vw solid #281853;
+        border-right: 35vw solid transparent;
+        filter: drop-shadow(0 0 1px white);
       }
 
       object {
         position: absolute;
-        top: 7vh;
-        left: 7vh;
+        top: 3.5vw;
+        left: 3.5vw;
         width: auto;
-        height: 25vh;
+        height: 12vw;
+      }
+    }
+
+    .menu {
+      padding: 25px;
+
+      a {
+        padding: 5px 15px;
+        margin: 10px 0;
+        border: 0 solid white;
+
+        &.r-line {
+           border-right-width: 1px;
+        }
+       
       }
     }
   }
@@ -108,11 +135,11 @@ export default {
   .heading {
     width: 100%;
     height: 100vh;
-    padding-top: 20vh;
+    padding-top: 25vh;
 
     h1 {
       font-family: "beon";
-      line-height: 0.2;
+      line-height: 0.5;
       font-size: 4.5em;
       text-shadow: 0 0 15px #E23EFF;
     }
@@ -120,16 +147,6 @@ export default {
 
   .scenter {
     text-align: center;
-  }
-
-  .logo {
-    text-align: right;
-
-    object {
-      width: auto;
-      height: 100%;
-      max-height: 250px;
-    }
   }
 </style>
 
