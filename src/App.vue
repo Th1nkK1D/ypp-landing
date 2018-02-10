4ad931005b0fa6c4d63fb342f11f891ffbf6c077
Withee Poositasai <witheep@gmail.com>
2018-02-10 17:52:40 +0700

    Rework event section

--- src/App.vue
+++ src/App.vue
@@ -2,10 +2,10 @@
   <div id="app">
     <!-- Navbar -->
     <div class="navbar">
-      <div class="icon">
+      <!-- <div class="icon">
         <div class="triangle"></div>
         <object type="image/svg+xml" data="/static/logoypp.svg"></object>
-      </div>
+      </div> -->
 
       <!-- Menu -->
       <div class="menu">
@@ -20,8 +20,7 @@
       <div class="columns is-gapless">
         <div class="column is-4"></div>
         <div class="column">
-          <h1>Young Passionate</h1>
-          <h1>Program</h1>
+          <h1>Young Passionate Program</h1>
 
           <p>Ignite your passion. Surround with young talent. Informative workshop from experience mentor.</p>
           <br>
@@ -32,14 +31,58 @@
     </div>
     <!-- End of Heading -->
 
-    <div class="scenter">
+    <div class="scenter timeline">
       <h2 class="hyellow">EVENTS</h2>
+      <div class="columns is-gapless is-mobile">
+        <div class="column is-6 dash d-bottom d-right"> </div>
+        <div class="column is-6"></div>
+      </div>
+      <div class="columns is-gapless dash d-bottom d-left">
+        <div class="column is-6">
+          <div class="event">
+            30 March - 1 April 2018
+            <p style="color: #eee93b">YOUNG LEAN ENTREPRENEUR</p>
+          </div>
+        </div>
+        <div class="column is-6"></div>
+      </div>
+      <div class="columns is-gapless dash d-bottom d-right">
+        <div class="column is-6"></div>
+        <div class="column is-6">
+          <div class="event">
+            6 April - 8 April 2018 
+            <p style="color: #f8a28c">YOUNG DIGITAL MARKETEER </p>
+          </div>
+        </div>
+      </div>
+      <div class="columns is-gapless dash d-bottom d-left">
+        <div class="column is-6">
+          <div class="event">
+            20 April - 22 April 2018
+            <p style="color: #97d3de">YOUNG INNOVATION CREATOR</p>
+          </div>
+        </div>
+        <div class="column is-6"></div>
+      </div>
+      <div class="columns is-gapless">
+        <div class="column is-6"></div>
+        <div class="column is-6 dash d-bottom d-right">
+          <div class="event">
+            June 2018
+            <p style="color: #77c8b9">YOUNG SOCIAL IMPACT MAKER</p>
+          </div>
+        </div>
+      </div>
+      <div class="columns is-gapless is-mobile">
+        <div class="column is-6"></div>
+        <div class="column is-6 dash d-left"></div>
+      </div>
     </div>
 
-    <div class="scenter">
+    <!-- <div class="scenter">
       <h2 class="hgreen">ABOUT US</h2>
       <p>Ever wondered how some graphic designers always manage to produce beautiful looking designs for their brochures, website designs, logo designs? Talent…yes, it helps but there are a handful of more important things you can do that will have even complete beginners producing award winning design.</p>
-    </div>
+    </div> -->
   </div>
 </template>
 
@@ -80,11 +123,12 @@ export default {
   .btn-prim {
     background: linear-gradient(-100deg, #00769b, #022088);;
     color: white;
-    font-size: 16px;
+    font-size: 1em;
     font-weight: bold;
     height: 50px;
     width: 200px;
     border-radius: 25px;
+    border: 1px solid white;
     filter: drop-shadow(2px 2px 5px rgba(0,0,0,0.5));
   }
 
@@ -134,14 +178,50 @@ export default {
 
   .heading {
     width: 100%;
-    height: 100vh;
+    height: 75vh;
     padding-top: 25vh;
 
     h1 {
       font-family: "beon";
-      line-height: 0.5;
       font-size: 4.5em;
       text-shadow: 0 0 15px #E23EFF;
+    }
+  }
+
+  .timeline {
+    padding: 0 10vw;
+
+    .columns {
+      margin: 0 !important;
+    }
+
+    .dash {
+      border: 0 dashed white;
+      box-sizing: border-box;
+      min-height: 20px;
+
+      &.d-right {
+        border-right-width: 1px;
+      }
+
+      &.d-left {
+        border-left-width: 1px;
+      }
+
+      &.d-bottom {
+        border-bottom-width: 1px;
+      }
+    }
+
+    .event {
+      padding: 20px 30px;
+      text-align: left;
+
+      p {
+        margin: 10px 0 0 0;
+        font-size: 1.8em;
+        line-height: 1.2;
+      }
     }
   }
 
